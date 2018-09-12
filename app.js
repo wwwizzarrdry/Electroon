9794fb0153845d6189db12fd6aa9efeba0c2726e
wwwizzarrdry <wwwizzarrdry@yahoo.com>
2018-09-12 23:34:12 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,176 @@
+const electron = require('electron');
+const {Menu, Tray, ipcMain } = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let tray = null;
+let contextMenu = null;
+let splash = null;
+let bgwin = null;
+let win = null;
+
+function createWindow () {
+    let screenElectron = electron.screen;
+    let mainScreen = screenElectron.getPrimaryDisplay();
+    let allScreens = screenElectron.getAllDisplays();
+    let dimensions = mainScreen.size;
+    let posX = (dimensions.width - 350);
+    let posY = (dimensions.height - 400);
+    console.log("\n dimensions: " + dimensions.width + "x" + dimensions.height);
+
+    // Create the browser window.
+    win = new BrowserWindow({
+        'node-integration': true,
+        x: posX,
+        y: posY,
+        width: 350, 
+        height: 365,
+        alwaysOnTop: false,
+        overlayScrollbars: true,
+        show: false,
+        transparent: true,
+        frame: false,
+        resizeable: false,
+        skipTaskbar: true,
+        maximizable: false,
+        minimizable: false
+    })
+
+    // and load the index.html of the app.
+    win.loadFile('index.html')
+  
+    // on first ready
+    win.once('ready-to-show', () => {       
+        setTimeout(function(){
+            splash.close();
+            win.show();
+            win.focus();
+            //win.webContents.openDevTools() // Open the DevTools.
+        }, 2000)
+    })
+
+    win.on('show', () => {
+        tray.setHighlightMode('always')
+    })
+
+    win.on('hide', () => {
+        tray.setHighlightMode('never')
+    })
+    
+    // Emitted when the window is closed.
+    win.on('closed', () => {
+        win = null
+    })
+}
+
+// Splash screen
+function createSplash() {
+    // Show the splash screen
+    splash = new BrowserWindow({
+        'node-integration': true,
+        position: "center",
+        width: 150, 
+        height: 150, 
+        show: false,
+        frame: false,  
+        movable: false,
+        alwaysOnTop: true,
+        transparent: true,
+        resizeable: false,
+        skipTaskbar: true,
+        maximizable: false,
+        minimizable: false,
+        focusable: false
+    });
+    
+    splash.loadFile('splash.html')
+    splash.show();
+}
+  
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', () => {
+    createSplash();
+    createWindow();
+
+    // Build tray icon
+    tray = new Tray('assets/img/roon.png');
+    contextMenu = Menu.buildFromTemplate([
+        {
+            label: 'Always On Top', 
+            type: 'checkbox', 
+            checked: win.isAlwaysOnTop(),
+            click: function() {
+                var alwaystop = win.isAlwaysOnTop() ? false : true;
+                win.setAlwaysOnTop(alwaystop);
+                this.checked = alwaystop;
+            }
+        },
+        {
+            label: 'Minimize', 
+            type: 'checkbox', 
+            checked: win.isMinimized(),
+            click: function() {
+                var minimized = win.isMinimized();
+                (minimized) ? win.restore() : win.minimize();
+                this.checked = !minimized;
+                this.label = (minimized) ? 'Minimize' : 'Restore';
+            }
+        },
+        {
+            label: 'Force Reload', 
+            type: 'normal', 
+            click: function() {
+                win.reload();
+            }
+        },
+        {
+            label: 'Inspect', 
+            type: 'normal', 
+            click: function(){
+                win.toggleDevTools();
+            }
+        },
+        {
+            label: 'Quit', 
+            type: 'normal',  
+            click: function() {
+                app.quit();
+            }
+        }
+    ]);
+    
+    tray.setToolTip('Roon Mini-Controller');
+    tray.setContextMenu(contextMenu);
+    tray.on('click', () => {
+        if(win.isVisible()) {
+            win.hide();
+        } else {
+            win.show();
+        } 
+    });
+
+    // Cross Window Comms
+    ipcMain.on('nowPlaying', (event, arg) => {
+        console.log(arg.zone)
+        //console.log("ipcMain.on('nowPlaying', (event, arg)\n", "event: ", event, "arg: ", arg);
+        //event.sender.send('nowPlaying', arg)
+        //bgwin.webContents.send('nowPlaying', arg);
+    })
+})
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+  
+app.on('activate', () => {
+    if (win === null) {
+        createWindow()
+    }
+})
---
+++ roon.js
@@ -0,0 +1,277 @@
+var RoonApi          = require("node-roon-api"),
+    RoonApiTransport = require('node-roon-api-transport'),
+    RoonApiImage     = require('node-roon-api-image'),
+    RoonApiBrowse    = require('node-roon-api-browse'),
+    RoonApiSettings  = require('node-roon-api-settings'),
+    Vue              = require('vue'),
+    settings         = require('./config.json');
+
+console.log("settings", settings)
+//var config = roon.load_config("config") || {};
+
+var core;
+var roon = new RoonApi({
+    extension_id:        'com.wwwizzarrdry.electroon',
+    display_name:        'Roon Mini Controller',
+    display_version:     "1.0.0",
+    publisher:           'wwwizzarrdry',
+    email:               '',
+
+    core_paired: function(core_) {
+        v.current_zone_id = roon.load_config("current_zone_id");
+        core = core_;
+        core.services.RoonApiTransport.subscribe_zones((response, msg) => {
+            if (response == "Subscribed") {
+                let zones = msg.zones.reduce((p,e) => (p[e.zone_id] = e) && p, {});
+                v.$set('zones', zones);
+            } else if (response == "Changed") {
+                var z;
+                if (msg.zones_removed) msg.zones_removed.forEach(e => delete(v.zones[e.zone_id]));
+                if (msg.zones_added)   msg.zones_added  .forEach(e => v.zones[e.zone_id] = e);
+                if (msg.zones_changed) msg.zones_changed.forEach(e => v.zones[e.zone_id] = e);
+                v.$set('zones', v.zones);
+            }
+        });
+        v.status = 'connected';
+
+        v.listoffset = 0;
+        refresh_browse();
+    },
+    core_unpaired: function(core_) {
+	core = undefined;
+        v.status = 'disconnected';
+    }
+});
+
+roon.init_services({
+    required_services:   [ RoonApiBrowse, RoonApiTransport, RoonApiImage ],
+});
+
+//Vue.config.devtools = true;
+var v = new Vue({
+    el: "#roonapp",
+    template: require('./player.html'),
+    data: function() { 
+        return {
+            server_ip: settings.server_ip || 'localhost',
+            server_port: settings.server_port || 9100,
+            status: 'null',
+            zones: [],
+            current_zone_id: null,
+            album_img: null,
+            artist_img: null,
+            listoffset: 0,
+            list: null,
+            items: [],
+            history: settings.history
+        }
+    },
+    computed: {
+        zone: function () {
+            return this.zones[this.current_zone_id];
+        },
+        artist_img: function() {
+            this.artist_img = 'http://' + this.server_ip + ':' + this.server_port + '/api/image/' + this.zones[this.current_zone_id].now_playing.artist_image_keys[0] + '?scale=fit&width=1920&height=1080';
+            return 'http://' + this.server_ip + ':' + this.server_port + '/api/image/' + this.zones[this.current_zone_id].now_playing.artist_image_keys[0] + '?scale=fit&width=1920&height=1080';
+        },
+        album_img: function() {
+            this.album_img = 'http://' + this.server_ip + ':' + this.server_port + '/api/image/' + this.zones[this.current_zone_id].now_playing.image_key + '?scale=fit&width=1920&height=1080';
+            return 'http://' + this.server_ip + ':' + this.server_port + '/api/image/' + this.zones[this.current_zone_id].now_playing.image_key + '?scale=fit&width=1920&height=1080';
+        }
+    },
+    watch: {
+        'current_zone_id': function(val, oldval) {
+            roon.save_config("current_zone_id", val);
+            refresh_browse();
+        },
+        'zones[current_zone_id].now_playing.image_key': function(img) {
+            var ts = new Date;
+            ipcRenderer.send('nowPlaying', {
+                "artist_img": this.artist_img,
+                "album_img": this.album_img,
+                "zone": this.zone
+            });
+            this.history.push({
+                timestamp: ts.valueOf(),
+                profile: this.zones[this.current_zone_id].settings.profile,
+                line1: this.zones[this.current_zone_id].now_playing.three_line.line1,
+                line1: this.zones[this.current_zone_id].now_playing.three_line.line1,
+                line1: this.zones[this.current_zone_id].now_playing.three_line.line1,
+                artist_img: this.artist_img,
+                album_img: this.album_img
+            });
+            roon.save_config("history", history);
+        }
+    },
+    methods: {
+        to_time: function (s) {
+            let r = "";
+            if (s >= 3600) {           
+                r += Math.floor(s / 3600).toString() + ":"; s = s % 3600; 
+            }
+
+            if (!r || s >= 600) {
+                r += Math.floor(s / 60).toString() + ":"; 
+                s = s % 60;  
+            } else {
+                r += "0"; 
+                r += Math.floor(s / 60).toString() + ":"; 
+                s = s % 60;   
+            }
+
+            if (s >= 10) {
+                r += s.toString();
+                s = -1;
+            } else if (s >= 0) { 
+                r += "0"; r += s.toString();
+                s = -1;
+            }
+            return r;
+        },
+        transport_change_settings: function(val) {
+            console.log(val);
+            if (val == "shuffle") {
+                this.zone.settings.shuffle = !this.zone.settings.shuffle;
+                $('.shuffle').find('i').removeClass('uk-text-primary md-color-white')
+                .addClass((this.zone.settings.shuffle ? 'uk-text-primary' : 'md-color-white'));
+            } else if (val == "auto_radio") {
+                this.zone.settings.auto_radio = !this.zone.settings.auto_radio;
+                $('.radio').find('i').removeClass('uk-text-primary md-color-white')
+                .addClass((this.zone.settings.auto_radio ? 'uk-text-primary' : 'md-color-white'));
+            } else if (val == "loop") {
+                this.zone.settings.loop = 'disabled'; // aint nobody got time for more loops...
+            };
+
+            core.services.RoonApiTransport.change_settings(this.zone, this.zone.settings, function(err){
+                console.log("Transport Error:\ncore.services.RoonApiTransport.change_settings()", err)
+            });
+        },         
+        transport_playpause: function() {
+            core.services.RoonApiTransport.control(this.zone, 'playpause');
+        },
+        transport_stop: function() {
+            core.services.RoonApiTransport.control(this.zone, 'stop');
+        },
+        transport_previous: function() {
+            core.services.RoonApiTransport.control(this.zone, 'previous');
+        },
+        transport_next: function() {
+            core.services.RoonApiTransport.control(this.zone, 'next');
+        },
+        transport_volume: function(el) {
+            var outs = this.zone.outputs; 
+            for(i=0;i<outs.length;i++){
+                core.services.RoonApiTransport.change_volume(outs[i].output_id, "absolute", $(el.target).val())
+            }
+        },
+        show_volume: function(el) {
+            if($(".volume-input").is(":visible")) {
+                $(".volume-input").slideToggle()
+            } else {
+                var outs = this.zone.outputs, avg = 0;            
+                // Normalize All Zone Volumes 
+                for(i=0;i<outs.length;i++){
+                    avg += outs[i].volume.value;
+                }
+                $(".volume-input").val((avg/outs.length).toFixed(0)).slideToggle().select().focus();
+            }
+        },
+        list_item: function(item) {
+            if (!item.input_prompt) {
+                refresh_browse({ item_key: item.item_key });
+            }
+        },
+        list_input: function(item) {
+            let val = (item.input_prompt.value || "").trim();
+            if (val === "") return;
+            if (item.input_prompt) {
+                refresh_browse({ item_key: item.item_key, input: val });
+            }
+        },
+        list_back: function() {
+            refresh_browse({ pop_levels: 1 });
+        },
+        list_home: function() {
+            refresh_browse({ pop_all: true });
+        },
+        list_refresh: function() {
+            refresh_browse({ refresh_list: true } );
+        },
+        list_next_page: function() {
+            load_browse(this.listoffset + 100);
+        },
+        list_prev_page: function() {
+            load_browse(this.listoffset - 100);
+        },
+        toggleList: function(){
+            $(".list, .lists").toggleClass("active");
+        },
+        toggleTrackAlbumTitle: function(){
+            $(".line1, .line2").toggleClass("uk-hidden");
+        }
+    }
+});
+
+function refresh_browse(opts) {
+    opts = Object.assign({
+        hierarchy:          "browse",
+        zone_or_output_id:  v.current_zone_id,
+    }, opts);
+
+    core.services.RoonApiBrowse.browse(opts, (err, r) => {
+        if (err) { console.log("Error: RoonApiBrowse", err, r); return; }
+
+        console.log(err, r);
+
+        if (r.action == 'list') {
+            v.$set("list", r.list);
+            v.$set("items", []);
+            var listoffset = r.list.display_offset > 0 ? r.list.display_offset : 0;
+            load_browse(listoffset);
+
+        } else if (r.action == 'message') {
+            alert((r.is_error ? "ERROR: " : "") + r.message);
+
+        } else if (r.action == 'replace_item') {
+            var i = 0;
+            var l = v.items;
+            while (i < l.length) {
+                if (l[i].item_key == opts.item_key) {
+                    l.splice(i, 1, r.item);
+                    break;
+                }
+                i++;
+            }
+            v.$set("items", l);
+
+        } else if (r.action == 'remove_item') {
+            var i = 0;
+            var l = v.items;
+            while (i < l.length) {
+                if (l[i].item_key == opts.item_key) {
+                    l.splice(i, 1);
+                    break;
+                }
+                i++;
+            }
+            v.$set("items", l);
+        }
+    });
+}
+
+function load_browse(listoffset) {
+    core.services.RoonApiBrowse.load({
+        hierarchy: "browse",
+        offset: listoffset,
+        set_display_offset: listoffset,
+    }, (err, r) => {
+        v.$set("listoffset", listoffset);
+        v.$set("items", r.items);
+    });
+}
+
+var go = function() {
+    v.status = 'connecting';
+    roon.connect_to_host(v.server_ip, v.server_port, v.server_port, () => setTimeout(go, 3000));
+};
+go();    
